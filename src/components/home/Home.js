a774da2e6121668412ecd0627413910fec469151
Htein Lin Aung <hteinlinaung1423@gmail.com>
2020-10-18 13:39:00 +0800

    Init project with default template

---
+++ src/components/home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const HomeApp = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+
+};
+export { HomeApp as default }
+
+// Handle window resize event
+//window.addEventListener('resize', () => handleResize());
+
+//const handleResize = () => {
+//    // Get the initial dementions for the application
+//    const domElement = document.querySelector('#root');
+//    console.log("offsetWidth ", domElement.offsetWidth);
+//    setInitWidth(domElement.offsetWidth);
+//}
