97dbd09e317213e2c39e7d79c5ca720383494eb3
Htein Lin Aung <hteinlinaung1423@gmail.com>
2020-10-18 15:15:57 +0800

    added hook usage

--- src/actions/home-actions/home-action.js
+++ src/actions/home-actions/home-action.js
@@ -6,10 +6,13 @@ export const getWeatherConditions = async () => {
         const result = await axios.get('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast');
         const weatherRes = JSON.parse(result.request.response)
 
-        return {
-            current_datetime: weatherRes.items[0].timestamp,
-            condtion: weatherRes.items[0].forecasts[11].forecast
-        }
+        //return {
+        //    current_datetime: weatherRes.items[0].timestamp,
+        //    condtion: weatherRes.items[0].forecasts[11].forecast
+        //}
+
+        return weatherRes.items[0];
+
     } catch (error) {
         console.error(error);
     }
